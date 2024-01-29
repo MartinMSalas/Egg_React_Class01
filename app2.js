d45352015e73fc34112a749d44e1edba874fa1bc
MartinMSalas <ingmartinmsalas@gmail.com>
2024-01-29 20:41:48 -0300

    adding files

---
+++ app2.js
@@ -0,0 +1,12 @@
+let porcentajeDescuento = 0;
+
+let cantidadMillas = prompt("Ingrese la cantidad de millas");
+
+if (cantidadMillas > 30000) {
+    porcentajeDescuento = 20;	
+} else if (cantidadMillas > 5000) {
+    porcentajeDescuento = 10;	
+} else {
+    porcentajeDescuento = 0;
+}
+alert(`Tu descuento es ${porcentajeDescuento}%`);
---
+++ script/script.js
@@ -0,0 +1,31 @@
+// Espera a que el DOM esté cargado
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtén la referencia al elemento <p> con el id "mensaje"
+    let mensajeElement = document.getElementById("mensaje");
+  
+    // Cambia el contenido del elemento <p>
+    mensajeElement.textContent = "El enlace entre HTML y JavaScript en el header ha sido exitoso!";
+  });
+  
+  var name = prompt("Hola, ingresa tu nombre");
+  var yearOfBirth = prompt("Hola, ingresa tu año de nacimiento");
+  var monthOfBirth = prompt("Hola, ingresa tu mes de nacimiento");
+  var dayOfBirth = prompt("Hola, ingresa tu dia de nacimiento");
+  
+  // Create a new Date object for the current date
+  var currentDate = new Date();
+  
+  // Create a new Date object for the date of birth
+  // Subtract 1 from the month because JavaScript counts months from 0 to 11
+  var birthDate = new Date(yearOfBirth, monthOfBirth - 1, dayOfBirth);
+  
+  // Calculate the difference in years
+  var age = currentDate.getFullYear() - birthDate.getFullYear();
+  
+  // If the current month and day are before the month and day of birth, subtract 1 from the age
+  if (currentDate.getMonth() < birthDate.getMonth() || 
+      (currentDate.getMonth() == birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  
+  console.log(`Hola ${name}, tu edad es ${age} años.`);
